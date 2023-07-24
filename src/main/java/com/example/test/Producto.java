c7170884eb65c34ee862fb7e8c38033b4026874d
JeffersonOnsihuay <129435467+JeffersonOnsihuay@users.noreply.github.com>
2023-07-24 15:33:07 +0000

    ejecutable

--- src/main/java/com/example/test/Producto.java
+++ src/main/java/com/example/test/Producto.java
@@ -1,5 +1,46 @@
 package com.example.test;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private double precio;
+    
+    public Producto() {
+    }
+
+    public Producto(Long id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
     
 }
--- src/main/java/com/example/test/Usuario.java
+++ src/main/java/com/example/test/Usuario.java
@@ -1,5 +1,5 @@
 package com.example.test;
 
 public class Usuario {
-    
+
 }
