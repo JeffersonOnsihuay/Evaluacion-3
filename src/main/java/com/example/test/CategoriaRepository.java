00d1aad6828881247a09e487a1098c9a51c2f921
JeffersonOnsihuay <129435467+JeffersonOnsihuay@users.noreply.github.com>
2023-07-24 18:04:46 +0000

    evc3_final_database_rest Resource

--- src/main/java/com/example/test/CategoriaRepository.java
+++ src/main/java/com/example/test/CategoriaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "categoria", path = "categoria")
+@RepositoryRestResource(collectionResourceRel = "categorias", path = "categorias")
 public interface CategoriaRepository extends PagingAndSortingRepository<Categoria, Integer>,CrudRepository<Categoria, Integer>{
     List<Categoria> findByNombre(@Param("nombre") String nombre);
 }
--- src/main/java/com/example/test/InstrumentoRepository.java
+++ src/main/java/com/example/test/InstrumentoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "instrumento", path = "instrumento")
+@RepositoryRestResource(collectionResourceRel = "instrumentos", path = "instrumentos")
 public interface InstrumentoRepository extends PagingAndSortingRepository<Instrumento, Integer>,CrudRepository<Instrumento, Integer> {
     List<Instrumento> findByNombre(@Param("nombre") String nombre);
 }
